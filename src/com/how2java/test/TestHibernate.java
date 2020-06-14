de2fac46d07fd4b7e9a5b85d150c7bf7be5d81b5
ZhaiDongboBug <18747867831@163.com>
2020-06-14 10:16:46 +0800

    框架学习-Hibernate-1
    总结：
    配置之后报出异常
    org.hibernate.HibernateException: /hibernate.cfg.xml not found
    仔细检查后并没有发现哪里错误
    最后将站长的xml文件内容复制过来才运行成功
    猜想应该是某一个字符错误引起的

---
+++ src/com/how2java/test/TestHibernate.java
@@ -0,0 +1,28 @@
+package com.how2java.test;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+import com.how2java.pojo.Product;
+
+public class TestHibernate {
+
+	public static void main(String[] args) {
+		
+		//
+		SessionFactory sf = new Configuration().configure().buildSessionFactory();
+		Session s = sf.openSession();
+		s.beginTransaction();
+		
+		Product p = new Product();
+		p.setName("iphone6");
+		p.setPrice(6000);
+		s.save(p);
+		
+		s.getTransaction().commit();
+		s.close();
+		sf.close();
+	}
+
+}
