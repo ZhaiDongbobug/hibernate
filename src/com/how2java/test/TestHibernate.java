6fc26f02a967053098ec99e041b20a6c926c50c2
ZhaiDongboBug <18747867831@163.com>
2020-06-16 11:52:30 +0800

    框架学习-Hibernate-13
    总结：
    1.c3p0配置过程？
    hibernate.cfg.xml配置

--- src/com/how2java/test/TestHibernate.java
+++ src/com/how2java/test/TestHibernate.java
@@ -23,34 +23,52 @@ public class TestHibernate {
 	public static void main(String[] args) throws InterruptedException {
 		
 	SessionFactory sf = new Configuration().configure().buildSessionFactory();
-	Session s1 = sf.openSession();
-	Session s2 = sf.openSession();
 	
-	s1.beginTransaction();
-	s2.beginTransaction();
+	Session s = sf.openSession();
+	s.beginTransaction();
 	
-	Product p1 = (Product) s1.get(Product.class, 2);
-	System.out.println("产品原本价格是：" + p1.getPrice());
+	s.createQuery("from Category").list();
 	
-	p1.setPrice(p1.getPrice()+1000);
-	
-	Product p2 = (Product) s2.get(Product.class, 2);
-	p2.setPrice(p2.getPrice() + 1000);
-		
-	s1.update(p1);
-	s2.update(p2);
-	
-	s1.getTransaction().commit();
-	s2.getTransaction().commit();
-		
-	Product p = (Product) s1.get(Product.class, 2);
-	
-	System.out.println("经过两次价格增加后，价格变为：" + p.getPrice());
-	
-	s1.close();
-	s2.close();
-	
+	s.getTransaction().commit();
+	s.close();
 	sf.close();
+	
+	
+	
+	
+	
+	
+	
+	
+	
+//	Session s1 = sf.openSession();
+//	Session s2 = sf.openSession();
+//	
+//	s1.beginTransaction();
+//	s2.beginTransaction();
+//	
+//	Product p1 = (Product) s1.get(Product.class, 2);
+//	System.out.println("产品原本价格是：" + p1.getPrice());
+//	
+//	p1.setPrice(p1.getPrice()+1000);
+//	
+//	Product p2 = (Product) s2.get(Product.class, 2);
+//	p2.setPrice(p2.getPrice() + 1000);
+//		
+//	s1.update(p1);
+//	s2.update(p2);
+//	
+//	s1.getTransaction().commit();
+//	s2.getTransaction().commit();
+//		
+//	Product p = (Product) s1.get(Product.class, 2);
+//	
+//	System.out.println("经过两次价格增加后，价格变为：" + p.getPrice());
+//	
+//	s1.close();
+//	s2.close();
+//	
+//	sf.close();
 		
 		
 		
